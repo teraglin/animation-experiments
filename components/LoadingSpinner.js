7f5b31127242e889ef64776de4bdb8f3e51bfa64
Aidan Kirvan <apkirvan@hotmail.com>
2022-02-06 18:57:38 +1100

    Task: created loading spinner component

---
+++ components/LoadingSpinner.js
@@ -0,0 +1,16 @@
+export const LoadingSpinner = () => {
+  return (
+    <div
+      className="loading-spinner"
+    >
+      <div className="spinner-section-1">
+        <div className="spinner-section-2"></div>
+        <div className="spinner-section-2"></div>
+      </div>
+      <div className="spinner-section-1">
+        <div className="spinner-section-2"></div>
+        <div className="spinner-section-2"></div>
+      </div>
+    </div>
+  )
+}
